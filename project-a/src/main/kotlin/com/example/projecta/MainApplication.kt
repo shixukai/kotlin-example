976aceeea7edd705ad8d836eab872985e199235d
Xukai.Shi <xukai.shi@hcrobots.com>
2024-05-22 13:08:39 +0800

    修复编译问题

--- project-a/src/main/kotlin/com/example/projecta/MainApplication.kt
+++ project-a/src/main/kotlin/com/example/projecta/MainApplication.kt
@@ -2,10 +2,8 @@ package com.example.projecta
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.example.projecta", "com.example.projectb", "com.example.common"])
 class MainApplication
 
 fun main(args: Array<String>) {
